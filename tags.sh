e159f696c2f7135b106bafccca7693e6564fc07f
Andrey Popov <rainfinder@gmail.com>
2013-07-08 12:41:52 +0200

    CMSSW tags have been updated to PAT recommendations
    The tags have been updated according to [1]. Also LumiDB tag is updated to the latest one. Other tags (for b-tagging, MET corrections, etc.) have not been checked and and might also require an update w.r.t. to release versions. The code is compiled without errors, but it have not been run.
    [1] https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePATReleaseNotes52X#V08_09_62

--- tags.sh
+++ tags.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
 
-# Use the latest 53X release recommended by PAT. However switch to 5_3_7_patch5 is recommended for
-# HCAL laser pollusion
-# https://twiki.cern.ch/twiki/bin/view/CMS/PdmVKnowFeatures#HCAL_laser_events_in_prompt_2012
-release="CMSSW_5_3_7_patch4"
+# Use the latest release recommended for 2012 data
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePATRecipes#CMSSW_5_3_X_pro2012
+release="CMSSW_5_3_11"
 scramv1 project CMSSW $release  # cmsrel alias expanded
 
 cd $release/src
 eval `scramv1 runtime -sh`  # this is cmsenv alias expanded
 
-# Use this interface to determine which tags are included in the release:
+# Use this interface to determine which tags are included in the release if needed:
 # https://cmstags.cern.ch/tc/#Releases
 
 
-# The latest PAT release V08-09-51
+# Tags for the latest PAT release V08-09-62. Tags recommended for tau reconstruction are ignored
+#as tau leptons are not used in the analysis
 # https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePATReleaseNotes52X
-addpkg DataFormats/PatCandidates V06-05-06-05
-addpkg PhysicsTools/PatAlgos     V08-09-51
-addpkg DataFormats/StdDictionaries V00-02-14
-addpkg FWCore/GuiBrowsers V00-00-70
-
-# Dependency-induced check-out
+addpkg DataFormats/PatCandidates V06-05-06-12
+addpkg PhysicsTools/PatAlgos     V08-09-62
+addpkg PhysicsTools/PatUtils
+#^ The last command checks a package out of release. It must be recompiled due to dependencies of
+#other packages. See an example in
 # https://hypernews.cern.ch/HyperNews/CMS/get/physTools/3021/1.html
-cvs co -r $release PhysicsTools/PatUtils
+
+# Latest b-tagging (recommended in the PAT recipe cited above)
+addpkg RecoBTag/ImpactParameter V01-04-09-01
+addpkg RecoBTag/SecondaryVertex V01-10-06
+addpkg RecoBTag/SoftLepton      V05-09-11
+addpkg RecoBTau/JetTagComputer  V02-03-02
+addpkg RecoBTag/Configuration   V00-07-05
+
+# Latest EGM isolation (recommended in the PAT recipe cited above)
+addpkg RecoParticleFlow/PFProducer V15-02-06
+
 
 # Latest tag for pixel-based luminosity
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/LumiCalc
-addpkg RecoLuminosity/LumiDB   V04-02-07
+addpkg RecoLuminosity/LumiDB   V04-02-08
+
+
+
+
+
+# WHAT IS BELOW THIS LINE HAS NOT BEEN CHECKED
+#-------------------------------------------------------------------------------
 
 # MET type-I and type-0 corrections
 # https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookMetAnalysis#Type_I_0_with_PAT
@@ -42,8 +58,8 @@ addpkg RecoLuminosity/LumiDB   V04-02-07
 
 # The latest data file for MET phi modulation (Run2012 A+B+C). However one shall check out the
 # whole python subdirectory as the python configuration fragments will be searched for locally
-cvs co -r $release JetMETCorrections/Type1MET/python
-cvs co -r 1.6 JetMETCorrections/Type1MET/python/pfMETsysShiftCorrections_cfi.py
+#cvs co -r $release JetMETCorrections/Type1MET/python
+#cvs co -r 1.6 JetMETCorrections/Type1MET/python/pfMETsysShiftCorrections_cfi.py
 
 # The file with the jet resolution must be available locally (corresponds to CMS AN-2011/330)
 #cvs co -r $release PhysicsTools/PatUtils/data/pfJetResolutionMCtoDataCorrLUT.root
@@ -51,28 +67,25 @@ cvs co -r 1.6 JetMETCorrections/Type1MET/python/pfMETsysShiftCorrections_cfi.py
 
 # MET filters
 # https://twiki.cern.ch/twiki/bin/view/CMS/MissingETOptionalFilters
-cvs co -r V00-00-13 RecoMET/METFilters
-cvs co -r V00-00-08 RecoMET/METAnalyzers
+#cvs co -r V00-00-13 RecoMET/METFilters
+#cvs co -r V00-00-08 RecoMET/METAnalyzers
 #cvs co -r V01-00-11-01 DPGAnalysis/Skims  # superceded by HCAL laser filer's dependencies
-cvs co -r V00-11-17 DPGAnalysis/SiStripTools
+#cvs co -r V00-11-17 DPGAnalysis/SiStripTools
 
 # HCAL-laser-polluted event filter requires CMSSW_5_3_7_patch5. These are the relevant packages
 # https://twiki.cern.ch/twiki/bin/view/CMS/PdmVKnowFeatures#HCAL_laser_events_in_prompt_2012
-cvs co -r V01-00-03-10 EventFilter/HcalRawToDigi
-cvs co -r V01-00-15 DPGAnalysis/Skims
+#cvs co -r V01-00-03-10 EventFilter/HcalRawToDigi
+#cvs co -r V01-00-15 DPGAnalysis/Skims
 
 # MVA electron ID in PAT and effective-area rho correction to the isolation
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/TwikiTopRefHermeticTopProjections?rev=4#Electrons
 # https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentification#MVA_based_Id_in_PAT
 # https://twiki.cern.ch/twiki/bin/view/CMS/TWikiTopRefEventSel?rev=178#Electrons
-cvs co -r V00-00-31-EA02 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
-cd EGamma/EGammaAnalysisTools/data
-cat download.url | xargs wget
-cd -
+#cvs co -r V00-00-31-EA02 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
+#cd EGamma/EGammaAnalysisTools/data
+#cat download.url | xargs wget
+#cd -
 
-
-# WHAT IS BELOW THIS LINE HAS NOT BEEN CHECKED
-#-------------------------------------------------------------------------------
 
 # B-tagging
 # https://hypernews.cern.ch/HyperNews/CMS/get/btag/879.html
