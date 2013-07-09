40b27ef13deff5b31e255f4a4ea8d5a318191e40
Andrey Popov <rainfinder@gmail.com>
2013-07-09 14:17:23 +0200

    Tags for MET have been updated
    The tags have been updated following recommendations from [1].
    [1] https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMETRecipe53X#CMSSW_5_3_11

--- tags.sh
+++ tags.sh
@@ -40,7 +40,7 @@ addpkg RecoLuminosity/LumiDB   V04-02-08
 
 
 # Recommendations for electron MVA ID
-# https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentification#Recipe_for_53X
+# https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentification?rev=45#Recipe_for_53X
 addpkg RecoEgamma/EgammaTools       V09-00-01
 addpkg EgammaAnalysis/ElectronTools SE_PhotonIsoProducer_MovedIn
 cvs co -r SE_PhotonIsoProducer_MovedOut -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
@@ -50,61 +50,28 @@ cat download.url | xargs wget
 cd -
 
 
-# Latest MET phi-modulation corrections. Althogh a single file is needed, one has to check out the
-# whole python directory since python configuration fragments will be searched for locally
-cvs co -r $release JetMETCorrections/Type1MET/python
+# MET tags
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMETRecipe53X?rev=14#CMSSW_5_3_11
+addpkg RecoMET/METAnalyzers V00-00-08
+addpkg DataFormats/METReco V03-03-11-01
+addpkg JetMETCorrections/Type1MET V04-06-09-02
+
+
+# Latest MET phi-modulation corrections. The enclosing subpackage has been checked out as a part of
+# MET tags. If it were not the case (i.e. if the release contained the recommended version of the
+# subpackage), it would not be sufficient to check out the configuration fragment with corrections
+# only but the whole python directory. The reason is that modules imported in this fragment are
+# searched for locally.
+#cvs co -r $release JetMETCorrections/Type1MET/python
 cvs co -r 1.7 JetMETCorrections/Type1MET/python/pfMETsysShiftCorrections_cfi.py
 
 
 # MET filters
 # https://twiki.cern.ch/twiki/bin/view/CMS/MissingETOptionalFilters?rev=61#A_Central_Filter_Package_RecoMET
 #addpkg RecoMET/METFilters       V00-00-13-01  # same version in the release
-addpkg RecoMET/METAnalyzers     V00-00-08
+#addpkg RecoMET/METAnalyzers     V00-00-08  # already check out as a part of MET tags
 #addpkg CommonTools/RecoAlgos    V00-03-23  # a newer version in the release
 #addpkg DPGAnalysis/Skims        V01-00-11-01  # a newer version in the release
 addpkg DPGAnalysis/SiStripTools V00-11-17
 #addpkg DataFormats/TrackerCommon V00-00-08  # same version in the release
 #addpkg RecoLocalTracker/SubCollectionProducers V01-09-05  # same version in the release
-
-
-
-# WHAT IS BELOW THIS LINE HAS NOT BEEN CHECKED
-#-------------------------------------------------------------------------------
-
-# MET type-I and type-0 corrections
-# https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookMetAnalysis#Type_I_0_with_PAT
-#addpkg CommonTools/ParticleFlow V00-03-16  # same version in the release
-#addpkg CommonTools/RecoAlgos V00-03-23  # newer version in the release
-#addpkg CommonTools/RecoUtils V00-00-13  # newer version in the release
-#addpkg PhysicsTools/PatAlgos V08-09-23  # newer version checked out for PAT
-#addpkg PhysicsTools/PatUtils V03-09-23  # newer version in the release
-#addpkg DataFormats/ParticleFlowCandidate V15-03-03  # newer version in the release
-#addpkg DataFormats/TrackReco V10-02-02  # same version in the release
-#addpkg DataFormats/VertexReco V02-00-04  # same version in the release
-#addpkg DataFormats/StdDictionaries V00-02-14  # same version checked out for PAT
-
-# The latest data file for MET phi modulation (Run2012 A+B+C). However one shall check out the
-# whole python subdirectory as the python configuration fragments will be searched for locally
-#cvs co -r $release JetMETCorrections/Type1MET/python
-#cvs co -r 1.6 JetMETCorrections/Type1MET/python/pfMETsysShiftCorrections_cfi.py
-
-# The file with the jet resolution must be available locally (corresponds to CMS AN-2011/330)
-#cvs co -r $release PhysicsTools/PatUtils/data/pfJetResolutionMCtoDataCorrLUT.root
-#^ Aldready checked out
-
-
-# HCAL-laser-polluted event filter requires CMSSW_5_3_7_patch5. These are the relevant packages
-# https://twiki.cern.ch/twiki/bin/view/CMS/PdmVKnowFeatures#HCAL_laser_events_in_prompt_2012
-#cvs co -r V01-00-03-10 EventFilter/HcalRawToDigi
-#cvs co -r V01-00-15 DPGAnalysis/Skims
-
-
-# B-tagging
-# https://hypernews.cern.ch/HyperNews/CMS/get/btag/879.html
-#~addpkg RecoBTag/PerformanceDB  V01-03-14
-#~addpkg CondFormats/PhysicsToolsObjects V01-03-12  # needed by the above package
-
-
-# The latest PU reweighting
-# https://twiki.cern.ch/twiki/bin/view/CMS/PileupMCReweightingUtilities#Reweighting_Using_PhysicsTools_U
-#~addpkg PhysicsTools/Utilities  V08-03-17
