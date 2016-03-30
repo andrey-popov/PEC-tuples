9177235737e00b031707fb27dffd769c637a95c6
Andrey Popov <rainfinder@gmail.com>
2016-03-30 17:01:10 +0200

    Apply the fix for ShiftedPATJetProducer
    The old bug is still there...

--- cmssw.sh
+++ cmssw.sh
@@ -7,9 +7,10 @@ scramv1 project CMSSW $release  # cmsrel alias expanded
 cd $release/src
 eval `scramv1 runtime -sh`  # this is cmsenv alias expanded
 
-exit 0
-# Code below has not been updated
-
 
 # Bugfixes in CMSSW
-git cms-merge-topic andrey-popov:fix-ShiftedJetProducerT
+git cms-init
+git remote add private-updates https://github.com/andrey-popov/cmssw.git
+git fetch private-updates
+git cms-addpkg PhysicsTools/PatUtils
+git cherry-pick fcb419ef5d4c2e2b7c1410f4d9d8621bf641d524  # From branch fix-ShiftedJetProducerT
