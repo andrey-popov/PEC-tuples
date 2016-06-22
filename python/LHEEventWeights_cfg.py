132b649b44f92ec7545d5f3e2dd594f79ac428ed
Andrey Popov <rainfinder@gmail.com>
2016-06-22 16:47:15 +0200

    Improve command-line arguments in LHEEventWeights_cfg.py
    Use standard options provided by VarParsing as in commit [1] and allow
    to configure label of LHE info product.
    [1] 9d79044c5651c1acf8f210d0d65c8a3177bc9fea

--- python/LHEEventWeights_cfg.py
+++ python/LHEEventWeights_cfg.py
@@ -32,27 +32,28 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing('python')
 
 options.register(
-    'inputFile', '', VarParsing.multiplicity.singleton, VarParsing.varType.string,
-    'Name of the input file (comma-separared list is also allowed)'
-)
-options.register(
     'storeWeights', False, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
     'Enables storing of event weights in a ROOT tree.'
 )
 options.register(
-    'outputName', 'eventWeights', VarParsing.multiplicity.singleton, VarParsing.varType.string,
-    'Name of the output ROOT file.'
+    'labelLHEInfoProduct', 'externalLHEProducer', VarParsing.multiplicity.singleton,
+    VarParsing.varType.string, 'Label to access LHEEventProduct'
 )
+
+# Override defaults for automatically defined options
+options.setType('outputFile', VarParsing.varType.string)
+options.setDefault('outputFile', 'eventWeights.root')
 
 options.parseArguments()
 
 
 # Specify the input file
-if len(options.inputFile) == 0:
+if len(options.inputFiles) == 0:
     raise RuntimeError, 'No input file is provided'
 
 process.source = cms.Source('PoolSource',
-    fileNames = cms.untracked.vstring(options.inputFile.split(',')))
+    fileNames = cms.untracked.vstring(options.inputFiles)
+)
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxEvents))
 
@@ -60,15 +61,20 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxEv
 # Service to handle the output file
 if options.storeWeights:
     postfix = '_' + string.join([random.choice(string.letters) for i in range(3)], '')
-
+    
+    if options.outputFile.endswith('.root'):
+        outputBaseName = options.outputFile[:-5] 
+    else:
+        outputBaseName = options.outputFile
+    
     process.TFileService = cms.Service('TFileService',
-        fileName = cms.string(options.outputName + postfix + '.root'))
+        fileName = cms.string(outputBaseName + postfix + '.root'))
 
 
 # The plugin to read and store weights
 process.lheEventWeights = cms.EDAnalyzer('LHEEventWeights',
-    lheRunInfoProduct = cms.InputTag('externalLHEProducer'),
-    lheEventInfoProduct = cms.InputTag('externalLHEProducer'),
+    lheRunInfoProduct = cms.InputTag(options.labelLHEInfoProduct),
+    lheEventInfoProduct = cms.InputTag(options.labelLHEInfoProduct),
     generator = cms.InputTag('generator'),
     weightsHeaderTag = cms.string('initrwgt'),
     rescaleLHEWeights = cms.bool(True),
