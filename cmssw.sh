ce5bf3dc8094abde67a89f9ea228a3c91de96eec
Andrey Popov <rainfinder@gmail.com>
2016-08-31 14:37:34 +0200

    Change CMSSW release suitable for MET recipes
    Switch to an older CMSSW release and merge MET updates following the
    discussion here [1]. It seems this also includes updated tunning for MET
    significance, although there are some further updates not included yet.
    [1] https://hypernews.cern.ch/HyperNews/CMS/get/met/478.html

--- cmssw.sh
+++ cmssw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# The latest release in the 80X series
-release="CMSSW_8_0_18"
+# Release supported for MET
+release="CMSSW_8_0_11"
 scramv1 project CMSSW $release  # cmsrel alias expanded
 
 cd $release/src
@@ -10,6 +10,10 @@ eval `scramv1 runtime -sh`  # this is cmsenv alias expanded
 
 git cms-init
 
-# Additional MET filters [1]
-# [1] https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2?rev=99#How_to_run_the_Bad_Charged_Hadro
+# Updates for MET [1]
+# [1] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=52#Instructions_for_8_0_X_X_11
+git cms-merge-topic cms-met:metTool80X
+
+# Additional MET filters [2]
+# [2] https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2?rev=99#How_to_run_the_Bad_Charged_Hadro
 git cms-merge-topic -u cms-met:CMSSW_8_0_X-METFilterUpdate
