a148e8501bbdc13d148f8a7ba169f2daf84ecc65
Andrey Popov <rainfinder@gmail.com>
2016-12-09 15:20:22 +0100

    Technical migration to a new 80X release
    This commit starts development for Summer16 simulation and
    re-reconstructed 2016 data. Updated package will be released in the
    3.3.X series.
    
    Version of CMSSW has been updated, including merged updates for MET.
    Code compiles and runs without errors, producing PEC tuples with
    reasonable content.

--- cmssw.sh
+++ cmssw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Release supported for MET
-release="CMSSW_8_0_11"
+# Recent 80X release
+release="CMSSW_8_0_24"
 scramv1 project CMSSW $release  # cmsrel alias expanded
 
 cd $release/src
@@ -11,9 +11,9 @@ eval `scramv1 runtime -sh`  # this is cmsenv alias expanded
 git cms-init
 
 # Updates for MET [1]
-# [1] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=52#Instructions_for_8_0_X_X_11
-git cms-merge-topic cms-met:metTool80X
+# [1] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=57#Instructions_for_8_0_X_X_20_for
+git cms-merge-topic cms-met:METRecipe_8020
 
 # Additional MET filters [2]
-# [2] https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2?rev=99#How_to_run_the_Bad_Charged_Hadro
-git cms-merge-topic -u cms-met:CMSSW_8_0_X-METFilterUpdate
+# [2] https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2?rev=103#How_to_run_the_Bad_Charged_Hadro
+git cms-merge-topic -u cms-met:fromCMSSW_8_0_20_postICHEPfilter
