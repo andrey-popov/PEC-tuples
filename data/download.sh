0ba5073d1660aea0ae6b73a66a623d0538c8f9dc
Andrey Popov <rainfinder@gmail.com>
2017-01-23 14:50:27 +0100

    Add commands to download JEC SQLite files
    Do it for the sake of completness of forthcoming release.

--- data/download.sh
+++ data/download.sh
@@ -3,6 +3,11 @@
 set -e
 
 
+# Files with preliminary JEC
+wget https://github.com/cms-jet/JECDatabase/raw/master/SQLiteFiles/Summer16_23Sep2016AllV2_DATA.db
+wget https://github.com/cms-jet/JECDatabase/raw/master/SQLiteFiles/Summer16_23Sep2016V2_MC.db
+
+
 # AFS directory containing additional data files for the PEC-tuples package
 DISTRIBUTION_DIR=/afs/cern.ch/work/a/aapopov/public/distribution/PECTuples/
 
