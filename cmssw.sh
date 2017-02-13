3bcbe470d65db37d7772fa6b21fd789e1a7538b5
Andrey Popov <rainfinder@gmail.com>
2017-02-13 18:06:29 +0100

    Technical migration to CMSSW_8_0_26_patch1
    Electron ID is supposed to be fully integrated in CMSSW [1], although
    wiki pages with instructions have not been updated yet and still
    recommend merging additional branches.
    [1] https://hypernews.cern.ch/HyperNews/CMS/get/egamma/1861/1.html

--- cmssw.sh
+++ cmssw.sh
@@ -1,46 +1,22 @@
 #!/bin/bash
 
 # Recent 80X release
-release="CMSSW_8_0_24"
+release="CMSSW_8_0_26_patch1"
 scramv1 project CMSSW $release  # cmsrel alias expanded
 
 cd $release/src
 eval `scramv1 runtime -sh`  # this is cmsenv alias expanded
 
 
-# Initial build.  Needed to create $CMSSW_BASE/external area, which is used by the recipe for
-# electron MVA ID.
-scram b
-
-
 git cms-init
 
-# Updates for MET [1]
-# [1] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=57#Instructions_for_8_0_X_X_20_for
+# Updates for MET [1]. They are contain updates for MET filters as well [2].
+# [1] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=60#Instructions_for_8_0_X_X_26_patc
+# [2] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=60#Important_Note
 git cms-merge-topic cms-met:METRecipe_8020
 
-# Additional MET filters [2]
-# [2] https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2?rev=103#How_to_run_the_Bad_Charged_Hadro
-git cms-merge-topic -u cms-met:fromCMSSW_8_0_20_postICHEPfilter
-
-# Updated cut-based electron ID [3]
-# [3] https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2?rev=41#Recipe_for_regular_users_for_8_0
-git cms-merge-topic ikrav:egm_id_80X_v2
-
-
-# Updated general-purpose MVA-based ID [4].  Note that code needs to be compiled for the directory
-# $CMSSW_BASE/external to be created.  When running jobs in CRAB one needs to set
-# config.JobType.sendExternalFolder = True.
-# [4] https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2?rev=30#Recipes_for_regular_users_common
-cd $CMSSW_BASE/external/$SCRAM_ARCH/
-git clone https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
-cd data/RecoEgamma/ElectronIdentification/data
-git checkout egm_id_80X_v1
-cd $CMSSW_BASE/src
-
-
-# DeepCSV b-tagger [5]
-# [5] https://twiki.cern.ch/twiki/bin/viewauth/CMS/DeepFlavour?rev=6
+# DeepCSV b-tagger [3]
+# [3] https://twiki.cern.ch/twiki/bin/viewauth/CMS/DeepFlavour?rev=6
 git cms-merge-topic -u mverzett:DeepFlavour-from-CMSSW_8_0_21
 mkdir RecoBTag/DeepFlavour/data
 cd !$
