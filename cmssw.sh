7b73a7b5746a54a39737071c129e633dddb7754e
Andrey Popov <rainfinder@gmail.com>
2017-06-23 12:42:53 +0200

    Technical migration to CMSSW_8_0_28
    There is no need to merge anything for DeepCSV anymore [1].
    [1] https://hypernews.cern.ch/HyperNews/CMS/get/btag/1423/1/1/1.html
    
    Only verify that the software can be built. Updates are needed to
    produce correct results.

--- cmssw.sh
+++ cmssw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Recent 80X release
-release="CMSSW_8_0_26_patch1"
+release="CMSSW_8_0_28"
 scramv1 project CMSSW $release  # cmsrel alias expanded
 
 cd $release/src
@@ -10,16 +10,6 @@ eval `scramv1 runtime -sh`  # this is cmsenv alias expanded
 
 git cms-init
 
-# Updates for MET [1]. They are contain updates for MET filters as well [2].
-# [1] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=64#Instructions_for_8_0_X_X_26_patc
-# [2] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=64#Important_Note
-git cms-merge-topic -u cms-met:METRecipe_8020
-git cms-merge-topic -u cms-met:METRecipe_80X_part2
-
-# DeepCSV b-tagger [3]
-# [3] https://twiki.cern.ch/twiki/bin/viewauth/CMS/DeepFlavour?rev=6
-git cms-merge-topic -u mverzett:DeepFlavour-from-CMSSW_8_0_21
-mkdir RecoBTag/DeepFlavour/data
-cd !$
-wget http://home.fnal.gov/~verzetti//DeepFlavour/training/DeepFlavourNoSL.json
-cd $CMSSW_BASE/src
+# Latest updates for MET for the legacy re-reconstruction [1]
+# [1] https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription?rev=66#Instructions_for_8_0_X_X_28_for
+git cms-merge-topic -u cms-met:METRecipe_8020_for80Xintegration
